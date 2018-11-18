629fdabdcc2f74b05e59ab27509f1f94667010f8
Charles Garcia Tobin <charles.garciatobin@gmail.com>
2018-11-18 21:14:28 +0000

    game logic basically in place, but:
    * bug when getting past second belt
    * missing belts past yellow
    * missing reload behaviour when you get to last belt
    * missing game over logic when you run out of hitters

--- taek_game.js
+++ taek_game.js
@@ -27,44 +27,17 @@ SOFTWARE.
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-/*
-var x = 0;
-var y = 0;
-*/
-
-var allwords = [];
-
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-function getPair() {
-    belt = taek_data[getRandomArbitrary(0,taek_data.length)];
-    word = belt.words[getRandomArbitrary(0,belt.words.length)];
-    orig = Object.keys(word)[0]
-    trans = Object.values(word)[0]
-    tran = trans[getRandomArbitrary(0,trans.length)];
-    return [orig,tran];
-}
-
-function getRandWords(different,count,arr) {
-    words = allwords;
-    ix = words.indexOf(different);
-    console.assert(ix != -1);
-    console.assert(count < words.length);
-    for (i = 0; i < count ; i++) {
-	ix = getRandomArbitrary(0,words.length);
-	arr.push(words[ix]);
-	words.splice(ix,1);
-    }
-}
-
-class shape {
-    constructor(x,y,w,h) {
+ 
+class Shape {
+    constructor(x,y,w,h,add = true) {
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
+	if (add) Shape.shapes.push(this);
     }
 
     set(x,y,w,h) {
@@ -93,78 +66,33 @@ class shape {
 	return false;
     }
 }
-shape.shapes = [];
+Shape.shapes = [];
 
-class LImage extends shape {
-
-    constructor(src,x,y,w,h) {
+class TextStr extends Shape {
+    constructor(word,x,y,w,h, add = true, fontsize = 40, font = "Verdana") {
 	super(x,y,w,h);
-	this.currImg = 0;
-	this.imgs = [];
-	for (var s of src) {
-	    let img = new Image();
-	    img.src = s;
-	    this.imgs.push(img);
-	}
-	this.numImgs = this.imgs.length;
-	shape.shapes.push(this);
-    }
-
-    draw(dctx = ctx) {
-	dctx.drawImage(this.imgs[this.currImg],this.x,this.y,this.w,this.h);
-    }
-
-}
-
-
-class textBrick extends LImage {
-    constructor(word,x,y,w,h) {
-	super(["./sprites/brick.png",
-	       "./sprites/brick_broken1.png",
-	       "./sprites/brick_broken2.png",
-	       "./sprites/brick_broken3.png",
-	       "./sprites/brick_broken4.png",
-	       "./sprites/brick_broken5.png"],
-	      x,y,w,h);
 	this.text = word;
-	this.fontSize = 40;
-	this.font = "Verdana";
+	this.fontSize = fontsize;
+	this.font = font;
 	this.align = "middle";
-	this.scrollBox = { width : 10, color : "#78a"};
 	this.fontset = false;
 	this.marginx = Math.ceil(this.w*0.1);
 	this.maxTextWidth = this.w - 2*this.marginx;
 	this.maxTextHeight = Math.floor(this.h/3);
 	this.marginy = Math.floor(this.h/3);
 	this.setFont();
-	this.hit = false;
-	textBrick.bricks.push(this);
-	if (typeof(textBrick.offscreenCanvas) == 'undefined') {
-	    textBrick.offscreenCanvas = document.createElement('canvas');
-	    textBrick.offscreenCanvas.width = this.w;
-	    textBrick.offscreenCanvas.height = this.h;
-	}
     }
-    
+
     setFont() {
         this.fontStr = "bolder " + this.fontSize + "px " + this.font;
         this.textHeight = this.fontSize + Math.ceil(this.fontSize * 0.05);
     }
-    
-    draw() {
-	let os = false; // offscreen defaults to false
-	let dctx = ctx;
-	let x = this.x;
-	let y = this.y;
-	let end = this.x+this.w;
-	if (end > canvas.width) {
-	    os = true; // if won't fit use offscreen
-	    dctx = textBrick.offscreenCanvas.getContext("2d");
-	    this.x = 0; // hacky temporarily set coords to 0;
-	    this.y = 0; 
-	}
-	
-	super.draw(dctx);
+
+    setWord(word) {
+	this.text = word;
+    }
+
+    draw(dctx = ctx) {
 	dctx.fillStyle='white';
 	do {
 	    this.setFont();
@@ -180,7 +108,73 @@ class textBrick extends LImage {
 	    }
 	} while (!this.fontset);
 	dctx.fillText(this.text,this.x+this.marginx,this.y+this.marginy);
+    }
+}
 
+class LImage extends Shape {
+
+    constructor(src,x,y,w,h) {
+	super(x,y,w,h);
+	this.currImg = 0;
+	this.imgs = [];
+	for (var s of src) {
+	    let img = new Image();
+	    img.src = s;
+	    this.imgs.push(img);
+	}
+	this.numImgs = this.imgs.length;
+    }
+
+    draw(dctx = ctx) {
+	dctx.drawImage(this.imgs[this.currImg],this.x,this.y,this.w,this.h);
+    }
+
+}
+
+class TextBrick extends LImage {
+    constructor(word,x,y,w,h) {
+	super(["./sprites/brick.png",
+	       "./sprites/brick_broken1.png",
+	       "./sprites/brick_broken2.png",
+	       "./sprites/brick_broken3.png",
+	       "./sprites/brick_broken4.png",
+	       "./sprites/brick_broken5.png"],
+	      x,y,w,h);
+	this.textStr = new TextStr(word,x,y,w,h,false);
+	this.hit = false;
+	if (typeof(TextBrick.offscreenCanvas) == 'undefined') {
+	    TextBrick.offscreenCanvas = document.createElement('canvas');
+	    TextBrick.offscreenCanvas.width = this.w;
+	    TextBrick.offscreenCanvas.height = this.h;
+	}
+    }
+    
+    setWord(word) {
+	this.textStr.setWord(word);
+    }
+    
+    draw() {
+	let os = false; // offscreen defaults to false
+	let dctx = ctx;
+	let x = this.x;
+	let y = this.y;
+	let end = this.x+this.w;
+
+	if (end > canvas.width) {
+	    os = true; // if won't fit use offscreen
+	    dctx = TextBrick.offscreenCanvas.getContext("2d");
+	    this.x = 0; // hacky temporarily set coords to 0;
+	    this.y = 0; 
+	}
+	
+	super.draw(dctx);
+	if (!this.hit) this.textStr.draw(dctx);
+
+	if (this.hit) {
+	    console.log("Hello");
+	}
+
+	
 	if (os) { // drawing offscreen
 	    this.x = x;
 	    this.y = y;
@@ -194,31 +188,31 @@ class textBrick extends LImage {
 	    ctx.putImageData(rightimg,this.x,this.y);
 	    ctx.putImageData(leftimg,0,this.y);
 	}
-	
     }
 
     animate(t) {
-	if (textBrick.v == 0 && this.hit) {
-	    if (!textBrick.last) textBrick.last = t;
-	    let p = t - textBrick.last;
+	if (Game.theGame.state ==1 && this.hit) {
+	    if (!TextBrick.last) TextBrick.last = t;
+	    let p = t - TextBrick.last;
 	    if (p > 250) {
-		textBrick.last = t;
+		TextBrick.last = t;
 		this.currImg++;
 		if (this.currImg == this.numImgs) {
 		    this.currImg = 0;
 		    this.hit = false;
+		    Game.theGame.setState(0);
 		}
 	    }
 	}
-	this.x = (this.x + textBrick.v) % canvas.width;
+	this.x = (this.x + TextBrick.v) % canvas.width;
+	this.textStr.x = this.x;
     }
 }
 
-textBrick.bricks = [];
-textBrick.v = 2;  // 0 if we hit the right brick
-textBrick.last = null;
+TextBrick.v = 2;  // 0 if we hit the right brick
+TextBrick.last = null;
 
-class hitter extends LImage {
+class Hitter extends LImage {
     constructor(top,backh) {
 	super(["./sprites/fist.png",
 	       "./sprites/fisthurt.png",
@@ -230,54 +224,45 @@ class hitter extends LImage {
 	      Math.floor(backh/3));
 
 	this.v = 0;
-	this.hitpointx = this.x+Math.floor(0.75*this.w);
+	this.hitpointx = [this.x+Math.floor(0.75*this.w),
+			  this.x+Math.floor(0.75*this.w),
+			  this.x+Math.floor(0.25*this.w),
+			  this.x+Math.floor(0.25*this.w)];
 	
 	this.top = top;
 	this.bottom = this.y;
 	this.keydown = false;
 	document.addEventListener('keydown', function(event) {
             if(event.key == ' ' ) {
-		hitter.keydown = true;
+		Hitter.keydown = true;
             }
             if(event.key == 's' ) {
-	     	textBrick.v++;
+	     	TextBrick.v++;
 	    }
             if(event.key == 'a' ) {
-	     	textBrick.v--;
+	     	TextBrick.v--;
 	    }
 	}, false);
 
 	document.addEventListener('keyup', function(event) {
             if(event.key == ' ' ) {
-		hitter.keydown = false;
+		Hitter.keydown = false;
             }
 	}, false);
     }
 
-    // draw(dctx = ctx) {
-    // 	super.draw(ctx);
-    // 	ctx.beginPath();
-    // 	ctx.arc(this.hitpointx,this.y,5,0,2*Math.PI);
-    // 	ctx.stroke();
-    // }
-
-
     animate(t) {
-	if (hitter.keydown && this.v == 0) {
+	if (Hitter.keydown && this.v == 0 && Game.theGame.state == 0) {
 	    this.v = -20;
 	}
 
 	if (this.v != 0 ) // we're animating
 	{
 	    this.y += this.v;
-
-	    for (var b  of textBrick.bricks) {
-		if (b.intersect(this.hitpointx,this.y)) {
-		    textBrick.v = 0;
-		    b.hit = true;
-		    this.currImg++;
-		    if (this.currImg == this.numImgs) this.currImg = 0;
-		}
+	    let hit = Game.theGame.checkHit(this);
+	    if (hit == -1) {
+		this.currImg++;
+		if (this.currImg == this.numImgs) this.currImg = 0;
 	    }
 	    
 	    if (this.v < 0) {
@@ -295,85 +280,219 @@ class hitter extends LImage {
 	}
     }
 }
-hitter.keydown = false;
+Hitter.keydown = false;
 
 
-class belt extends shape{
-    constructor(col,x,y,w,h) {
+class Belt extends Shape {
+
+    constructor(belt,x,y,w,h) {
 	super(x,y,w,h);
-	this.col = col;
+	this.belt = belt;
+	let marginy = 5;
+	this.by = [this.y+Math.floor(this.h/marginy)];
+	this.bh = Math.floor((this.h-(marginy-2)*Math.floor(this.h/marginy))/3);
+	this.by.push(this.by[0]+this.bh);
+	this.by.push(this.by[0]+2*this.bh);
+	this.textStr = new TextStr("Kup: "+belt.kup,
+				   x,this.by[0]+3*this.bh,
+				   w,this.y+this.h-(this.by[0]+3*this.bh),false);
+    }
+
+    setBelt(belt) { this.belt = belt; }
+    
+    draw(dctx = ctx) {
+	// first 1/3 of belt
+	dctx.fillStyle = 'red'; 
+	dctx.fillRect(this.x,this.by[0]-2,this.w,2);
+	dctx.fillStyle = this.belt.col[0]; 
+	dctx.fillRect(this.x,this.by[0],this.w,this.bh);
+	// second 1/3 of belt
+	dctx.fillStyle = this.belt.col[1]; 
+	dctx.fillRect(this.x,this.by[1],this.w,this.bh);
+	// last 1/3 of belt
+	dctx.fillStyle = this.belt.col[2]; 
+	dctx.fillRect(this.x,this.by[2],this.w,this.bh);
+	dctx.fillStyle = 'red'; 
+	dctx.fillRect(this.x,this.by[2]+this.bh,this.w,2);
+	// draw text
+	this.textStr.draw(dctx);
     }
 }
 
-var nwords = 5;
-var keydown = false;
+class Game {
 
-function doInit() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    x = canvas.width/2;
-    y = canvas.height-30;
-    console.log(taek_data.length);
-    for (i in taek_data) {
-	belt = taek_data[i];
-	for (w in belt.words) {
-	    word = belt.words[w];
-	    orig = Object.keys(word)[0]
-	    trans = Object.values(word)[0]
-	    for (t in trans) {
-		allwords.push(trans[t]);
+    constructor(nwords = 5) {
+	if (Game.theGame) return Game.theGame;
+	Game.theGame = this;
+	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let x = canvas.width/2;
+	let y = canvas.height-30;
+	console.log(taek_data.length);
+	this.allwords = taek_data[0].words;
+	this.bwords = [];
+	this.orig = "";
+	this.trans = "";
+	this.nwords = nwords;
+	// states =
+	// 0 - words moving ready to hit
+	// 1 - hit correct word, word disintegrating
+	//     input disallowed, on end new words are selected
+	this.state = 0; 
+
+	this.hits = 0;
+	this.hitsToPass = 5; // hits to move onto next belt
+	
+	this.brickw = Math.floor(canvas.width/nwords);
+	this.brickh = Math.floor(canvas.height/10);
+
+	let backgroundh = canvas.height-this.brickh-1;
+	let backgroundy = this.brickh+1;
+
+	new LImage(
+	    ["./sprites/background.png"],
+	    0, 
+	    this.brickh,
+	    Math.floor(canvas.width/3),
+	    Math.floor((canvas.height-this.brickh-1)/3)
+	);
+
+	new LImage(
+	    ["./sprites/background.png"],
+	    canvas.width-Math.floor(canvas.width/3), 
+	    this.brickh,
+	    Math.floor(canvas.width/3),
+	    Math.floor((canvas.height-this.brickh-1)/3)
+	);
+
+	this.beltix = 0;
+	this.belt = new Belt(taek_data[this.beltix],
+			canvas.width-Math.floor(canvas.width/3),
+			this.brickh+Math.floor((canvas.height-this.brickh-1)/3),
+			Math.floor(canvas.width/3),
+			Math.floor((canvas.height-this.brickh-1)/3)
+		       );
+    
+
+	this.hit  = new Hitter(backgroundy,backgroundh);
+
+	this.bricks = [];
+	
+	for (var i = 0 ; i < nwords ; i++) {
+     	    this.bricks.push(new TextBrick("",i*this.brickw, 0, this.brickw, this.brickh));
+	}
+
+	this.textOrig = new TextStr("",
+				    canvas.width-Math.floor(canvas.width/3),
+				    this.brickh+2*Math.floor((canvas.height-this.brickh-1)/3),
+				    Math.floor(canvas.width/3),
+				    Math.floor((canvas.height-this.brickh-1)/3));
+	this.setWords();
+    }
+
+    setWords() {
+	// get first word out
+	let cwords = this.allwords;
+	let ix = getRandomArbitrary(0,cwords.length);
+	let rw = cwords[ix];
+	this.orig = Object.keys(rw)[0];
+	let transl = rw[this.orig];
+	this.trans = transl[getRandomArbitrary(0,transl.length)];
+	cwords.splice(ix,1);
+	let wl = []
+	wl.push(this.trans);
+	for (var i = 0; i < (this.nwords-1); i++) {
+	    ix = getRandomArbitrary(0,cwords.length);
+	    rw = cwords[ix];
+	    let k = Object.keys(rw)[0];
+	    transl = rw[k];
+	    wl.push(transl[getRandomArbitrary(0,transl.length)]);
+	    cwords.splice(ix,1);
+	}
+	// now set the words onto bricks
+	ix = 0;
+	while (wl.length) {
+	    let ix2 = getRandomArbitrary(0,wl.length);
+	    this.bricks[ix].setWord(wl[ix2]);
+	    wl.splice(ix2,1);
+	    ix++;
+	}
+	this.textOrig.setWord(this.orig);
+    }
+
+    // return 0 if hit target
+    // -1 if hit wrong target
+    // -2 if still flying
+    checkHit(hitter) {
+	for (var b  of this.bricks) {
+	    if (b.intersect(hitter.hitpointx[hitter.currImg],hitter.y)) {
+		if (b.textStr.text == this.trans) {
+		    b.hit = true;
+		    this.setState(1);
+		    return 0;
+		}
+		else {
+		    return -1;
+		}
 	    }
+	}
+	return -2;
+    }
+    
+
+    setState(state) {
+	if (state == 1 && this.state == 0) {
+	    this.oldV = TextBrick.v;
+	    TextBrick.v = 0; // stop bricks
+	    this.state = state;
+	    this.hits++;
+	    if (this.hits == this.hitsToPass) {
+		// belt up to more words required now
+		this.hitsToPass+=3;
+		this.hits = 0;
+		this.beltix++;
+		if (this.beltix == taek_data.length) {
+
+		}
+		this.belt.setBelt(taek_data[this.beltix]);
+		this.allwords.concat(taek_data[this.beltix].words);
+	    }
+	    return;
+	}
+	
+	if (state == 0 && this.state == 1) {
+	    this.setWords();
+	    TextBrick.v = this.oldV; // restart bricks
+	    for (var i = 0; i < this.bricks.length; i++) {
+     		this.bricks[i].set(this.brickw*i, 0, this.brickw, this.brickh);
+	    }
+	    this.state = state;
+	    return;
 	}
     }
 
-    brickw = Math.floor(canvas.width/nwords);
-    brickh = Math.floor(canvas.height/10);
-
-    backgroundh = canvas.height-brickh-1;
-    backgroundy = brickh+1;
-
-    new LImage(
-	["./sprites/background.png"],
-	0, 
-	brickh,
-	Math.floor(canvas.width/3),
-	Math.floor((canvas.height-brickh-1)/3)
-    );
-
-    new LImage(
-	["./sprites/background.png"],
-	canvas.width-Math.floor(canvas.width/3), 
-	brickh,
-	Math.floor(canvas.width/3),
-	Math.floor((canvas.height-brickh-1)/3)
-    );
-
-    hit  = new hitter(backgroundy,backgroundh);
-    
-    pair = getPair();
-    warr = [];
-    getRandWords(pair[1],nwords-1,warr);
-    ix = getRandomArbitrary(0,nwords);
-    warr.splice(ix,0,pair[1]);
-    
-    for (i = 0 ; i < nwords ; i++) {
-     	taek_word = new textBrick(warr[i],i*brickw, 0, brickw, brickh);
+    mainLoop(time) {
+	ctx.setTransform(1,0,0,1,0,0);
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.fillStyle="#000000";
+	ctx.fillRect(0,this.brickh,canvas.width,canvas.height-this.brickh);
+	for (var s of Shape.shapes) {
+	    s.animate(time);
+	    s.draw();
+	}
     }
+
+    
 }
+
+Game.theGame = null;
+new Game(5);
 
 function mainLoop(time){
-    ctx.setTransform(1,0,0,1,0,0);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle="#000000";
-    ctx.fillRect(0,brickh,canvas.width,canvas.height-brickh);
-    for (i in shape.shapes) {
-	shape.shapes[i].animate(time);
-	shape.shapes[i].draw();
-    }
+    Game.theGame.mainLoop(time);
     requestAnimationFrame(mainLoop);
 }
-
-doInit();
 
 requestAnimationFrame(mainLoop);
 
